c39beb3bb315ed7b30e6cfd907fa3646297eaddb
Ahmed Ateeb <114568734+ahmed-a133b@users.noreply.github.com>
2025-04-29 19:04:18 +0500

    Updated Backend/InventoryManager.java

--- Backend/InventoryManager.java
+++ Backend/InventoryManager.java
@@ -1,5 +1,7 @@
 package Backend;
 
+import Database.DBHandler;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -8,7 +10,8 @@ public class InventoryManager extends User
 	int IMId;
 	String Status;
 	ArrayList<Medicine> Medicines;
-	
+	DBHandler DB = DBHandler.getInstance();
+        
 	public InventoryManager()
 	{
 		Medicines = new ArrayList<Medicine>();
@@ -22,8 +25,18 @@ public class InventoryManager extends User
 		Status = status;
 		Medicines = medicines;
 	}
+        
+        public String getStatus()
+        {
+            return Status;
+        }
 	
-	public void AddMedicine(Medicine Med)
+        public int getImId()
+        {
+            return IMId;
+        }
+        
+	public void AddMedicine(Medicine Med) throws SQLException
 	{
 		for(Medicine Med1 : Medicines)
 		{
@@ -32,13 +45,26 @@ public class InventoryManager extends User
 				Med1.setStatus("Added");
 			}
 		}
-		//In DB (Update Status)
+		
+                DB.AddMedicineStore(Med);
 	}
 	
-	public void RemoveMedicine(Medicine Med)
+	public void RemoveMedicine(Medicine Med) throws SQLException
 	{
-		//In DB
-		Medicines.remove(Med);
+		for(Medicine Med1 : Medicines)
+		{
+			if(Med1 == Med)
+			{
+				Med1.setStatus("Approved");
+			}
+		}
+                DB.RemoveMedicine(Med);
+	}
+        
+        public void DeleteMedicine(Medicine Med) throws SQLException
+	{
+            DB.DeleteMedicine(Med);
+            Medicines = DB.GetAllMedicines();
 	}
 	
 	public ArrayList<Medicine> IdentifyExpired()
@@ -47,26 +73,26 @@ public class InventoryManager extends User
 		
 		for(Medicine Med: Medicines)
 		{
-			if(Med.getExpiry().isAfter(LocalDate.now()))
+                        if (Med.getExpiry().isBefore(LocalDate.now()) && !Med.getStatus().equalsIgnoreCase("Pending")) 
 			{
-				Expired.add(Med);
+                            Expired.add(Med);
 			}
 		}
 		
 		return Expired;
 	}
 	
-	public void Restock(Medicine Med, int quantity)
+	public void Restock(int MedId, int quantity) throws SQLException
 	{
 		for(Medicine Med1: Medicines)
 		{
-			if(Med1 == Med)
+			if(Med1.getMedicineId() == MedId)
 			{
-				int Quan = Med1.getQuantity();
-				Quan += quantity;
-				Med1.setQuantity(Quan);
-				//DB
-				break;
+                            int Quan = Med1.getQuantity();
+                            Quan += quantity;
+                            Med1.setQuantity(Quan);
+                            DB.RestockMedicine(Med1);
+                            break;
 			}
 		}
 	}
