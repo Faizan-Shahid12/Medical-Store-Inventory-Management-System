2924c5a8fa2a951deff76dee66c515b1174f5900
Ahmed Ateeb <114568734+ahmed-a133b@users.noreply.github.com>
2025-04-29 19:00:46 +0500

    Updated Backend/Supplier.java

--- Backend/Supplier.java
+++ Backend/Supplier.java
@@ -1,14 +1,22 @@
 package Backend;
 
+import Database.DBHandler;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Supplier extends User
 {
-	private int SupId;
+    private int SupId;
     private String LicenseNumber;
     private String Status = "Pending";
     private ArrayList<Medicine> SupMed;
+    private DBHandler DB = DBHandler.getInstance();
+    
+    public Supplier()
+    {
+        
+    }
     
     public Supplier(String Us, String Pass, String Nam, String Gender1, int age1, String Ema, LocalDate DateofBirth, int supId, String licenseNumber, String status) 
     {
@@ -19,76 +27,102 @@ public class Supplier extends User
 		SupMed = new ArrayList<>();
     }
     
-    public Medicine AddRequest(String Name, String Type, int quan,double price, LocalDate expiry, LocalDate Manufac)
+    public void AddRequest(String Name, String Type, int quan,double price, LocalDate expiry, LocalDate Manufac) throws SQLException
     {
 
-    	//DB.getLastId();
-    	int LastId = 0;
+    	int LastId = DB.getLastMedId();
     	LastId = LastId + 1;
-    	Medicine newMed = new Medicine(LastId,quan,price,expiry,Manufac,Name,"Pending","Stopped",Type,this);
+    	Medicine newMed = new Medicine(LastId,quan,price,expiry,Manufac,Name,"Pending","Stopped",Type,this.SupId);
     	SupMed.add(newMed);
-    	//In DB
+    	DB.AddMedicine(newMed);
     	
-    	return newMed;
     }
     
-    public void RemoveRequest(Medicine Med)
+    public void DeleteMedicine(Medicine Med) throws SQLException
+    {
+        SupMed.remove(Med);
+        DB.DeleteMedicine(Med);
+    }
+    
+    public void RemoveRequest(Medicine Med) throws SQLException
     {
     	SupMed.remove(Med);
-    	//In DB
+    	DB.RemoveMedicine(Med);
     }
     
-    public void StopSupply(Medicine Med)
+    public void StopSupply(Medicine Med) throws SQLException
     {
     	for (Medicine Med1: SupMed)
     	{
     		if(Med1 == Med)
     		{
     			Med1.SupplyStatus = "Stopped";
-    			//In DB
+                        DB.StopSupply(Med.getMedicineId());
     			break;
     		}
     	}
     	
     }
     
-    public void StartSupply(Medicine Med)
+    public void StartSupply(Medicine Med) throws SQLException
     {
     	for (Medicine Med1: SupMed)
     	{
     		if(Med1 == Med)
     		{
     			Med1.SupplyStatus = "Started";
-    			//In DB
-    			break;
+                        DB.StartSupply(Med.getMedicineId());
+                        break;
     		}
     	}
     }
     
-	public int getSupId()
-	{
-		return SupId;
-	}
-	public void setSupId(int supId)
-	{
-		SupId = supId;
-	}
-	public String getLicenseNumber() 
-	{
-		return LicenseNumber;
-	}
-	public void setLicenseNumber(String licenseNumber) 
-	{
-		LicenseNumber = licenseNumber;
-	}
-	public String getStatus() 
-	{
-		return Status;
-	}
-	public void setStatus(String status) 
-	{
-		Status = status;
-	}
+    public ArrayList<Medicine> GetPendingRequest()
+    {
+        ArrayList<Medicine> MedList = new ArrayList<Medicine>();
+        
+        for(Medicine Med : SupMed)
+        {
+            if(Med.getStatus().equalsIgnoreCase("Pending"))
+            {
+                MedList.add(Med);
+            }
+        }
+        
+        return MedList;
+        
+    }
     
-    
+    public int getSupId()
+    {
+            return SupId;
+    }
+    public void setSupId(int supId)
+    {
+            SupId = supId;
+    }
+    public String getLicenseNumber() 
+    {
+            return LicenseNumber;
+    }
+    public void setLicenseNumber(String licenseNumber) 
+    {
+            LicenseNumber = licenseNumber;
+    }
+    public String getStatus() 
+    {
+            return Status;
+    }
+    public void setStatus(String status) 
+    {
+            Status = status;
+    }
+    public void setSupMed(ArrayList<Medicine> SupMed1)
+    {
+            SupMed = SupMed1;
+    }
+    public ArrayList<Medicine> getSupMed()
+    {
+        return SupMed;
+    }
 }
